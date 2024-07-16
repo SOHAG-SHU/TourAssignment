112ab9619a3e03d1b86c8ea5bb561e5a18b1c4e2
SOHAG-SHU <sohag209366@gmail.com>
2024-07-16 12:03:35 +0600

    Problem 5

---
+++ src/GameScoreTracker.java
@@ -0,0 +1,30 @@
+public class GameScoreTracker {
+    public static void main(String[] args) {
+        int numberOfGames = 5; // Assuming there are 5 games
+        TeamScores team1 = new TeamScores(numberOfGames);
+        TeamScores team2 = new TeamScores(numberOfGames);
+
+        // Sample scores for team 1
+        team1.setScore(0, 10);
+        team1.setScore(1, 15);
+        team1.setScore(2, 20);
+        team1.setScore(3, 25);
+        team1.setScore(4, 30);
+
+        // Sample scores for team 2
+        team2.setScore(0, 12);
+        team2.setScore(1, 18);
+        team2.setScore(2, 24);
+        team2.setScore(3, 28);
+        team2.setScore(4, 35);
+
+        // Print scores for both teams
+        System.out.println("Scores for Team 1:");
+        team1.printScores();
+        System.out.println("Total score for Team 1: " + team1.getTotalScore());
+
+        System.out.println("\nScores for Team 2:");
+        team2.printScores();
+        System.out.println("Total score for Team 2: " + team2.getTotalScore());
+    }
+}
