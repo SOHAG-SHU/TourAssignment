0d8b931da4636e69d911e7b0b556c18862bcbcdb
SOHAG-SHU <sohag209366@gmail.com>
2024-07-16 12:08:15 +0600

    Problem 3

--- src/SeashellCollector.java
+++ src/SeashellCollector.java
@@ -1,20 +1,16 @@
 import java.util.Scanner;
-
 public class SeashellCollector {
     private int[] seashells;
     private String[] seashellTypes;
-
     public SeashellCollector(String[] types) {
         seashellTypes = types;
         seashells = new int[types.length];
     }
-
     public void collectSeashell(int index, int count) {
         if (index >= 0 && index < seashells.length) {
             seashells[index] += count;
         }
     }
-
     public void printTotalSeashells() {
         int total = 0;
         for (int seashell : seashells) {
@@ -22,7 +18,6 @@ public class SeashellCollector {
         }
         System.out.println("Total seashells collected: " + total);
     }
-
     public void printSeashells() {
         for (int i = 0; i < seashells.length; i++) {
             System.out.println(seashellTypes[i] + ": " + seashells[i]);
